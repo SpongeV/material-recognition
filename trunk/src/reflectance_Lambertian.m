839d34c8a0c9d0541264e7c83ddcb6189c3360cc
jturnhout <jturnhout@2c323ff9-9af1-9afa-3f5d-657bc186a8d1>
2011-04-16 14:36:24 +0000

    removed some bugs

--- trunk/src/reflectance_Lambertian.m
+++ trunk/src/reflectance_Lambertian.m
@@ -1,18 +1,33 @@
 function irr = reflectance_Lambertian(normal, albedo, light_dir)
 
+
+	light_dir(1) = deg2rad(light_dir(1));
+	light_dir(2) = deg2rad(light_dir(2));
+
 	light = [cos(double(light_dir(2)))*sin(double(light_dir(1))), ...
-			sin(double(light_dir(2)))*cos(double(light_dir(1))), ...
-			cos(double(light_dir(1)))];
-	light = light/norm(light);
+			 sin(double(light_dir(2)))*sin(double(light_dir(1))), ...
+			 cos(double(light_dir(1)))];
+		
+	light = light / norm(light);
 
-	ambient_coeff = 0;
-	diffuse_coeff = 1;
-	shade = light(1)*normal(1) + light(2)*normal(2) + light(3)*normal(3);
+	I_l = 1.0;
+	
+	
+% 	albedo
+	normal = normal(:);
+	normal(3) = abs(normal(3));
+	
+	ambient_coeff = 0.0;
+	diffuse_coeff = 1.0;
+% 	shade = light(1)*normal(1) + light(2)*normal(2) + light(3)*normal(3);
 
+	shade = dot(light, normal);
+	
+	
 	if (shade < 0.0) 
 		shade = 0;
 	end
 	
-	irr = (ambient_coeff + diffuse_coeff * shade) * albedo;
 	
+	irr = (ambient_coeff + diffuse_coeff * shade) * albedo * I_l;
 end
--- trunk/src/unitTestTextureSynthesis.m
+++ trunk/src/unitTestTextureSynthesis.m
@@ -1,8 +1,10 @@
-function unitTestTextureSynthesis()
+function unitTestTextureSynthesis(angle, azimuth)
 % 	close all;
 	im = {};
 
-	angle = 30;
+% 	angle	= 60;
+% 	azimuth = 300;
+	no = 4;
 	
 % 	directory = '../Photex/';
 % 	im{1} = '0.aaa.0.30.0.bmp'
@@ -10,11 +12,11 @@ function unitTestTextureSynthesis()
 % 	im{3} = '0.aaa.0.30.180.bmp'
 % 	im{4} = '0.aaa.0.30.270.bmp'
 
-% 	directory = '../Photex/';
-% 	im{1} = '1.acd.0.75.0.bmp'
-% 	im{2} = '1.acd.0.75.90.bmp'
-% 	im{3} = '1.acd.0.75.180.bmp'
-% 	im{4} = '1.acd.0.75.270.bmp'
+	directory = '../Photex/';
+	im{1} = '1.acd.0.60.30.bmp'
+	im{2} = '1.acd.0.60.120.bmp'
+	im{3} = '1.acd.0.60.210.bmp'
+	im{4} = '1.acd.0.60.300.bmp'
 
 % 	directory = '../DB_PHOTEX/';
 % 	im{1} = '3.vm32.0.75.0.bmp'
@@ -22,44 +24,50 @@ function unitTestTextureSynthesis()
 % 	im{3} = '3.vm32.0.75.180.bmp'
 % 	im{4} = '3.vm32.0.75.270.bmp'
 
-	directory = '../Photex/';
-	im{1} = '2.adg.0.30.0.bmp'
-	im{2} = '2.adg.0.30.90.bmp'
-	im{3} = '2.adg.0.30.180.bmp'
-	im{4} = '2.adg.0.30.270.bmp'
+% 	directory = '../Photex/';
+% 	im{1} = '2.adg.0.30.0.bmp'
+% 	im{2} = '2.adg.0.30.90.bmp'
+% 	im{3} = '2.adg.0.30.180.bmp'
+% 	im{4} = '2.adg.0.30.270.bmp'
 
 
 
 	[albedo, normals] = PhotometricStereo(im, directory, 0, 0);
 	
 	parts = strsplit('.',im{3});
-	original_image = imcrop(imread(strcat(directory,parts{2},'/',im{3})), [156 156 200 200]);
-% 	original_image = imread(strcat(directory,parts{2},'/',im{3}));
+	original_image = imcrop(imread(strcat(directory,parts{2},'/',im{no})), [156 156 200 200]);
 	
-	synthesized_img1 = zeros(size(albedo));
-	synthesized_img2 = zeros(size(albedo));
-	synthesized_img3 = zeros(size(albedo));
+	for n=angle(1):angle(2):angle(3)
+		for m=azimuth(1):azimuth(2):azimuth(3)
+		% 	original_image = imread(strcat(directory,parts{2},'/',im{3}));
 
-	for i=1:size(albedo,1)
-		for j=1:size(albedo,2)
-			irr1 = reflectance_Lambertian(normals(i,j,:), albedo(i,j), [angle 180]);
-			irr2 = reflectance_Phong(normals(i,j,:), albedo(i,j), [angle 180], [0 0]);
-			irr3 = reflectance_OrenNayar(normals(i,j,:), albedo(i,j), [angle 180], [0 0]);
-			synthesized_img1(i,j) = irr1;
-			synthesized_img2(i,j) = irr2;
-			synthesized_img3(i,j) = irr3;
+			synthesized_img1 = zeros(size(albedo));
+% 			synthesized_img2 = zeros(size(albedo));
+			synthesized_img3 = zeros(size(albedo));
+
+
+			for i=1:size(albedo,1)
+				for j=1:size(albedo,2)
+					irr1 = reflectance_Lambertian(normals(i,j,:), albedo(i,j), [n m]);
+% 					irr2 = reflectance_Phong(normals(i,j,:), albedo(i,j), [n m], [0 0]);
+					irr3 = reflectance_OrenNayar(normals(i,j,:), albedo(i,j), [n m], [0 0]);
+					synthesized_img1(i,j) = irr1;
+% 					synthesized_img2(i,j) = irr2;
+					synthesized_img3(i,j) = irr3;
+				end
+			end
+
+% 			synthesized_img1 = synthesized_img1 / max(synthesized_img1(:));
+% 			synthesized_img2 = synthesized_img2 / max(synthesized_img2(:));
+% 			synthesized_img3 = synthesized_img3 / max(synthesized_img3(:));
+
+			figure; 
+			subplot(2,2,1); imshow(original_image);
+			subplot(2,2,2); imshow(synthesized_img1,[]);
+% 			subplot(2,2,3); imshow(synthesized_img2);
+			subplot(2,2,4); imshow(synthesized_img3,[]);
 		end
 	end
-	
-	synthesized_img1 = synthesized_img1 / max(synthesized_img1(:));
-	synthesized_img2 = synthesized_img2 / max(synthesized_img2(:));
-	synthesized_img3 = synthesized_img3 / max(synthesized_img3(:));
-	
-	figure; 
-	subplot(2,2,1); imshow(original_image);
-	subplot(2,2,2); imshow(synthesized_img1);
-	subplot(2,2,3); imshow(synthesized_img2);
-	subplot(2,2,4); imshow(synthesized_img3);
 end
 
 % shafer reflection model
